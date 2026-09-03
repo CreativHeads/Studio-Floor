cedc5c8980b4cc71496414e4e2e7aea201e9b997
sinan-prvt <mohamedsinan9400@gmail.com>
2026-09-03 13:01:07 +0530

    fix(backend): allow image field to be writable by overriding to_representation instead of using SerializerMethodField

--- backend/apps/studios/serializers.py
+++ backend/apps/studios/serializers.py
@@ -2,18 +2,17 @@ from rest_framework import serializers
 from .models import StudioRoom
 
 class StudioRoomSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-
     class Meta:
         model = StudioRoom
         fields = '__all__'
 
-    def get_image(self, obj):
-        if obj.image:
-            # Cloudinary storage returns absolute URLs, but DRF might mess it up.
-            # This explicitly returns the raw Cloudinary URL.
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        if instance.image:
             try:
-                return obj.image.url
+                ret['image'] = instance.image.url
             except ValueError:
-                return None
-        return None
+                ret['image'] = None
+        else:
+            ret['image'] = None
+        return ret
