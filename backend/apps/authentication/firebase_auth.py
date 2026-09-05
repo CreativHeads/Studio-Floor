5d60f42d1907a2a1a05248e1b167f74e01a8bb2d
sinan-prvt <mohamedsinan9400@gmail.com>
2026-09-05 12:33:14 +0530

    Add FIREBASE_CREDENTIALS_JSON support for Render

--- backend/apps/authentication/firebase_auth.py
+++ backend/apps/authentication/firebase_auth.py
@@ -1,4 +1,5 @@
 import os
+import json
 import firebase_admin
 from firebase_admin import auth, credentials
 
@@ -6,9 +7,14 @@ def initialize_firebase():
     if not firebase_admin._apps:
         # Check if we have credentials in env vars
         firebase_cred_path = os.environ.get('FIREBASE_CREDENTIALS_PATH')
+        firebase_cred_json = os.environ.get('FIREBASE_CREDENTIALS_JSON')
         
         try:
-            if firebase_cred_path and os.path.exists(firebase_cred_path):
+            if firebase_cred_json:
+                cred_dict = json.loads(firebase_cred_json)
+                cred = credentials.Certificate(cred_dict)
+                firebase_admin.initialize_app(cred)
+            elif firebase_cred_path and os.path.exists(firebase_cred_path):
                 cred = credentials.Certificate(firebase_cred_path)
                 firebase_admin.initialize_app(cred)
             else:
